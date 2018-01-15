b309a5fdd94dbd17723d38a7bea60969fd740232
Jordan Gilliam <gilliamja.te@gmail.com>
2018-01-15 21:52:46 +0000

    signup component rough draft

--- convoy/src/index.js
+++ convoy/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
+
+import App from './components/App';
+import SignIn from './components/SignIn';
+import Signup from './components/SignUp';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebaseApp.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log('user has signed in or up', user);
+    } else {
+        console.log('user has signed out or still needs to sign in.');
+    }
+});
+
+
+
+ReactDOM.render(
+    <Router path = "/" history={browserHistory}>
+        <Route path="/app" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+    </Router>, document.getElementById('root')
+);
+    
 registerServiceWorker();
