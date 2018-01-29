d05d629aa64ef604ced599d334d74d22619caff5
Jim Gregory <gregory.jimr@gmail.com>
2018-01-29 01:41:15 +0000

    merge

--- convoy/src/components/SignUp/SignUp.jsx
+++ convoy/src/components/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { firebaseApp } from '../../firebase';
+import { firebaseApp, db, defaultAuth } from '../../firebase';
+import firebase from 'firebase';
 
 class SignUp extends Component {
     constructor(props) {
@@ -11,20 +12,48 @@ class SignUp extends Component {
             password: '',
             error: {
                 message: ''
-            }
+            },
+            user: firebase.auth.uid,
         };
+    }
+    //createUser requires createUser, handleCurrentUser and is called on signup
+    
+    
+    //function that should be called right after signup to create new profile
+    createUser = () => {
+        db.ref("/profiles").push({
+            convoys: false,
+            email: this.state.email,
+            username: this.state.username
+        });
+        
+        
+        // db.ref().child("/profiles" + this.state.email).update("convoys");
+        // //use the profile ID (email address) as a key to reference new user and add the UID to the profile
+        // db.ref().child("/profiles" + this.state.email).update({"UID": this.state.user});
+        // //use the same index to add the username to the profile
+        // db.ref().child("/profiles" + this.state.email).update({"username": this.state.username});
     }
     
     signUp = () => {
         // console.log('this.state', this.state);
         const { email, password } = this.state;
         firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+            .then(
+                this.createUser()
+            )
             .catch(error => {
                 console.log('error', error);
                 this.setState({error});
             });
+        this.state.handleCurrentUser;
+        this.createUser;
     }
 
+    //set current user to be the current UID
+    handleCurrentUser = () => {
+        this.setState({user: firebaseApp.firebase.auth().currentUser});
+    }
     
     componentDidMount() {
         document.body.className = 'bodyBackground';
@@ -78,6 +107,7 @@ class SignUp extends Component {
                                             className="btn btn-primary red"
                                             type="button"
                                             onClick={this.signUp}
+                                            //call create user here?
                                         > 
                                             SignUp
                                         </button>
