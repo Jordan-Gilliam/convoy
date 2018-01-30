1bd682c33ff53e3df8d70afb0d2e7000c94d3d3f
Morgan Bradley <mbradleystylist@gmail.com>
2018-01-30 02:08:14 +0000

    updated controller

--- controllers/sendGridController.js
+++ controllers/sendGridController.js
@@ -8,50 +8,36 @@ module.exports = {
   },
 
   sendEmail: function(req, res) {
-    const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    const request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: {
-        personalizations: [{
-          to: [{
-            email: 'mbradleystylist@gmail.com'
-          }],
-          subject: "You've been invited to join Convoy!"
-        }],
-        from: {
-          email: 'weliketocodethings@gmail.com'
-        },
-        content: [{
-          type: 'text/plain',
-          value: 'and easy to do anywhere, even with Node.js'
-        }]
-      }
-    });
+    // // SendGrid OPTION 2 - send multiple individual emails to multiple recipients*****************************************************************
+    // only one recipient email will show in the 'to' field
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    // With promise
-    sg.API(request)
-      .then(function(response) {
-        console.log(response.statusCode);
-        console.log(response.body);
-        console.log(response.headers);
+    const msg = {
+      to: ['gilliamja.te@gmail.com', 'isa.oambrosio@gmail.com', 'gregory.jimr@gmail.com', 'mbradleystylist@gmail.com'],
+      from: 'test@example.com',
+      subject: '{friend} has invited you to join Convoy!',
+      text: 'Hello and welcome to Convoy! Your friend {user} has invited you to join a convoy for your next trip. Click below to accept the invitation and sign up today. Convoy Description.',
+      html: '<button>Join the Convoy!</button>',
+    };
+    // send and sendMultiple methods return a Promise
+    // handle success and capture errors:
+    // **this is needed for all options
+    sgMail
+      .sendMultiple(msg)
+      .then(() => {
+        //Celebrate
+        console.log("email sent");
       })
-      .catch(function(error) {
-        // error is an instance of SendGridError
-        // The full response is attached to error.response
-        console.log(error.response.statusCode);
+      .catch(error => {
+
+        //Log friendly error
+        console.error(error.toString());
+
       });
 
-    // With callback
-    sg.API(request, function(error, response) {
-      if (error) {
-        console.log('Error response received');
-      }
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
-    res.send("done");
+      res.send("sent!");
+
   }
 };
 
@@ -81,38 +67,5 @@ module.exports = {
 
 //     API.saveConvoy
 
-//     const sgMail = require('@sendgrid/mail');
-//     // const sg = require("sendgrid")(SENDGRID_API_KEY);
-//     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-//     const msg = {
-//       to: ['gilliamja.te@gmail.com', 'isa.oambrosio@gmail.com', 'gregory.jimr@gmail.com', 'mbradleystylist@gmail.com'],
-//       from: 'test@example.com',
-//       subject: '{friend} has invited you to join Convoy!',
-//       text: 'Hello and welcome to Convoy! Your friend {user} has invited you to join a convoy for your next trip. Click below to accept the invitation and sign up today. Convoy Description.',
-//       html: '<button>Join the Convoy!</button>',
-//     };
-
-//     // send and sendMultiple methods return a Promise
-//     // handle success and capture errors:
-//     // **this is needed for all options
-//     sgMail
-//       .send(msg)
-//       .then(() => {
-//         //Celebrate
-//         console.log("email sent");
-//       })
-//       .catch(error => {
-
-//         //Log friendly error
-//         console.error(error.toString());
-
-//         // //Extract error msg
-//         const {message, code, response} = error;
-
-//         // //Extract response msg
-//         const {headers, body} = response;
-//       });
-
-//     sgMail.send(msg);
+//     
 // }
