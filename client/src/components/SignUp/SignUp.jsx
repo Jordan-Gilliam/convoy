30710c1e1f39773f6c312f24585fbee4a8fac306
Jordan Gilliam <gilliamja.te@gmail.com>
2018-01-30 17:41:37 +0000

    Revert "password confirm- confirmed working heh"
    This reverts commit eeb9bf821fc17a0615ad02fcf3eb4c4230057fdf.

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -36,9 +36,8 @@ class SignUp extends Component {
     
     signUp = () => {
         // console.log('this.state', this.state);
-        const { email, passwordInitial, passwordConfirm } = this.state;
-        if (passwordInitial === passwordConfirm) {
-        firebaseApp.auth().createUserWithEmailAndPassword(email, passwordInitial)
+        const { email, password } = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
             .then((user) =>
                 this.profileId(user)
             )
@@ -46,9 +45,6 @@ class SignUp extends Component {
                 console.log('error', error);
                 this.setState({error});
             });
-        } else {
-            alert("check password spelling and try again");
-        }
     }
     
     componentDidMount() {
