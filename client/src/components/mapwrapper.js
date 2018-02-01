21f53f6848ff96a7eb92d4579a5eda2b39a4af24
Jim Gregory <gregory.jimr@gmail.com>
2018-02-01 19:23:13 +0000

    convoy and uid are dynamic on mapwrapper

--- client/src/components/mapwrapper.js
+++ client/src/components/mapwrapper.js
@@ -7,22 +7,18 @@ import { firebaseApp, db } from '../firebase';
 import firebase from 'firebase';
 
 //...
-const user = {name: "You", lat: 37.779519, lng: -122.405640}
-
+let user = {name: "You", lat: 37.779519, lng: -122.405640}
+let latt = 100;
+let longg = "";
     
 export class MapContainer extends React.Component {
-    state = {
-        lat: "",
-        lng: "",
-        uid: ""
-    }
     constructor(props) {
         super(props)
     
     this.state = {
         users,
-        lat: 0,
-        lng: 0,
+        lat: latt,
+        lng: longg,
     }
     }
 
@@ -50,6 +46,8 @@ export class MapContainer extends React.Component {
         window.navigator.geolocation.getCurrentPosition(function(position) {
             console.log("uid?: ");
             console.log("latitude: ", position.coords.latitude, " longitude: ", position.coords.longitude);
+            latt = position.coords.latitude;
+            longg = position.coords.longitude;
             // this.setState({lat: position.coords.latitude});
             // this.setState({lng: position.coords.longitude});
             //sends lat and lng to current user at current convoy.  Need to get current user and convoy to do this dynamically
@@ -59,7 +57,14 @@ export class MapContainer extends React.Component {
             });
             //next, pull all members at same path and push {name: "name", lat: "lat", lng: "lng"} to array
             
-
+            // db.ref(`convoys/${convoy}/members`).on("value", (snapshot) => {
+            // const members = [];
+            // snapshot.forEach((data) => {
+            // console.log("data.key: " + data.key + " data.value: " + data.value);
+            // });
+            // console.log("snapshot: ", snapshot);
+            // console.log("JSON.stringify(snapshot): ", JSON.stringify(snapshot));
+        // });
         }); 
     }
     
@@ -67,6 +72,8 @@ export class MapContainer extends React.Component {
         const { currentPosition } = this.props;
         // console.log("position!: ", currentPosition || "not yet");
         this.handlePosition(this.props.uid, this.props.convoy);
+        // this.setState({lat: this.position.coords.latitude});
+        // this.setState({lng: this.position.coords.longitude});
 
  
 
